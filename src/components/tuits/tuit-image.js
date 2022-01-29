5f1bed8fe671bb4917cec873f8fd8466f90f4d8b
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-01-28 21:43:32 -0500

    second commit

--- src/components/tuits/tuit-image.js
+++ src/components/tuits/tuit-image.js
@@ -1,7 +1,7 @@
 const TuitImage = ({tuit}) => {
   return(
     <div className="position-relative">
-      <img src={`../images/${tuit.image}`}
+      <img src={`../images/${tuit.image}`} alt="description of image"
            className="mt-2 w-100 ttr-rounded-15px"/>
       {
         tuit['image-overlay'] &&
--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -6,7 +6,7 @@ const Tuit = ({tuit}) => {
   return(
     <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
       <div className="pe-2">
-        <img src={`../images/${tuit['avatar-logo']}`}
+        <img src={`../images/${tuit['avatar-logo']}`} alt="description of image"
              className="ttr-tuit-avatar-logo rounded-circle"/>
       </div>
       <div>
