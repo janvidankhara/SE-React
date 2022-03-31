ae6af0ec3957aa1653041989019b8dfaa24922ef
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-03-31 18:23:27 -0400

    adding dislikes tuit states

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
+ import * as dislikesService from "../../services/dislikes-service";
 import * as service from "../../services/tuits-service";
+
 const Tuits = ({tuits = [], refreshTuits}) => {
     const likeTuit = (tuit) =>
         likesService.userLikesTuit("me", tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e))
+    const dislikeTuit = (tuit) =>
+        dislikesService.userTogglesTuitDislikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e));
     const deleteTuit = (tid) =>
         service.deleteTuit(tid)
             .then(refreshTuits);
@@ -20,6 +26,7 @@ const Tuits = ({tuits = [], refreshTuits}) => {
                   <Tuit key={tuit._id}
                         deleteTuit={deleteTuit}
                         likeTuit={likeTuit}
+                        dislikeTuit={dislikeTuit}
                         tuit={tuit}/>)
             }
           </ul>
