7429888e4219580b0dcb15cf8b9aa66c57fe1763
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-03-31 21:55:08 -0400

    check before moving to show dislikes in profile

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -28,11 +28,11 @@ const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
           <span onClick={() => dislikeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.dislikes > 0 &&
-                  <i className="fas fa-thumbs-up me-1" style={{color: 'blue'}}></i>
+                  <i className="fas fa-thumbs-down me-1" style={{color: 'blue'}}></i>
               }
               {
                 tuit.stats && tuit.stats.dislikes <= 0 &&
-                  <i className="far fa-thumbs-up me-1"></i>
+                  <i className="far fa-thumbs-down me-1"></i>
               }
             {tuit.stats && tuit.stats.dislikes}
           </span>
